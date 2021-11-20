2d15e4fa4545f62af21f76699fc92a59f2607691
leslie521 <542476843@qq.com>
2021-11-20 13:34:09 +0800

    cgb2108IV_01-sca

--- 01-sca/sca-common/src/main/java/com/jt/common/cache/DefaultCache.java
+++ 01-sca/sca-common/src/main/java/com/jt/common/cache/DefaultCache.java
@@ -4,5 +4,5 @@ package com.jt.common.cache;
 
 //@Component
 public class DefaultCache {
-
+    
 }
--- 01-sca/sca-provider/src/test/java/com/jt/util/StringTests.java
+++ 01-sca/sca-provider/src/test/java/com/jt/util/StringTests.java
@@ -11,6 +11,9 @@ public class StringTests {
     @Test
     public void methodTest(){
         boolean empty = StringUtils.isEmpty("anita");
+        String str1 = "";
+        String str2 = "string";
+        System.out.println(str1.equals(str2));
         System.out.println(empty);
     }
 
