bc2f589953870fc64172eb4b8c9d2224136c47c6
leslie521 <542476843@qq.com>
2021-11-26 08:49:49 +0800

    cgb2108IV_01-sca

--- 01-sca/sca-provider/src/main/java/com/jt/provider/controller/ProviderAppController.java
+++ 01-sca/sca-provider/src/main/java/com/jt/provider/controller/ProviderAppController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RefreshScope
 @RestController
 public class ProviderAppController {
