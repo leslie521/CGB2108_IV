73c0eace6d3b05bf9083ccf347daafef612a9fad
leslie521 <542476843@qq.com>
2021-12-10 12:37:43 +0800

    cgb2108IV_01-sca

---
+++ 03-redis/sca-template/src/main/java/com/jt/redis/pojo/Blog.java
@@ -0,0 +1,34 @@
+package com.jt.redis.pojo;
+
+import java.io.Serializable;
+
+
+public class Blog implements Serializable {
+    private static final long serialVersionUID = -2488119077949701629L;
+    private Integer id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- 03-redis/sca-template/src/test/java/com/jt/TestRedisTemplate.java
+++ 03-redis/sca-template/src/test/java/com/jt/TestRedisTemplate.java
@@ -1,10 +1,17 @@
 package com.jt;
 
+import com.jt.redis.pojo.Blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+import java.util.Map;
 
 @SpringBootTest
 public class TestRedisTemplate {
@@ -15,7 +22,42 @@ public class TestRedisTemplate {
      * 具体方法，后续就可以基于这些方法实现对Redis数据库的读写操作了
      */
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate redisTemplate;//springboot工程启动时创建
+
+    @Test
+    void testHashBlog(){
+        ValueOperations ho = redisTemplate.opsForValue();
+        Blog blog = new Blog();
+        blog.setId(100);
+        blog.setName("andy");
+        ho.set("blog", blog);
+        blog =(Blog)ho.get("blog");
+        System.out.println(blog);
+    }
+
+    @Test
+    void testHashOper(){
+        HashOperations ho = redisTemplate.opsForHash();
+        ho.put("blog", "id", 100);
+        ho.put("blog", "name", "tony");
+//        ho.putAll();
+        Object name = ho.get("blog", "name");
+        System.out.println(name);
+        Map blog = ho.entries("blog");
+        System.out.println(blog);
+    }
+
+    @Test
+    void testStringOper(){
+//        redisTemplate.setKeySerializer(RedisSerializer.string());
+//        redisTemplate.setValueSerializer(RedisSerializer.string());
+        ValueOperations<String,String> vo = redisTemplate.opsForValue();
+//        vo.set("id", 100, Duration.ofSeconds(10));
+        vo.set("id", "100");
+        Object id = vo.get("id");
+//        vo.increment("id");
+        System.out.println(id);
+    }
 
     @Test
     void testGetConnection(){
