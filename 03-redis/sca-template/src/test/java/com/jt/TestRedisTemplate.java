9cf4d73cf2c179d9e30bc5e0cf05600d408ed75c
leslie521 <542476843@qq.com>
2021-12-11 15:20:15 +0800

    cgb2108IV_01-sca

--- 03-redis/sca-template/src/test/java/com/jt/TestRedisTemplate.java
+++ 03-redis/sca-template/src/test/java/com/jt/TestRedisTemplate.java
@@ -4,10 +4,9 @@ import com.jt.redis.pojo.Blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.time.Duration;
@@ -26,12 +25,12 @@ public class TestRedisTemplate {
 
     @Test
     void testHashBlog(){
-        ValueOperations ho = redisTemplate.opsForValue();
+        ValueOperations vo = redisTemplate.opsForValue();
         Blog blog = new Blog();
         blog.setId(100);
         blog.setName("hello world");
-        ho.set("blog", blog);
-        blog =(Blog)ho.get("blog");
+        vo.set("blog", blog);
+        blog =(Blog)vo.get("blog");
         System.out.println(blog);
     }
 
@@ -63,12 +62,50 @@ public class TestRedisTemplate {
         //直接对已存在key递增会有ERR value is not an integer or out of range
         //因为值已经采用了JDK默认的序列化方式进行了序列化存储
         //假如一定要这样写，就是可以对已存在的key进行底层，可以修改默认序列化方式
-        //vo.increment("id");
+//        vo.increment("id");
         //假如希望使用RedisTemplate对象直接实现递增操作，可以用一个不存在的key实现递增
         //调用increment方法时，假如key不存在会自动创建key(会基于JDK方式序列化),但值不会JDK序列化
         Long views = vo.increment("views");
         System.out.println(id);
         System.out.println(views);
+    }
+
+    @Test
+    void testListOper(){
+        //向list集合放数据
+        ListOperations listOperations = redisTemplate.opsForList();
+        listOperations.leftPush("lstKey1", "100"); //lpush
+        listOperations.leftPushAll("lstKey1", "200","300");
+        listOperations.leftPush("lstKey1", "100", "105");
+        listOperations.rightPush("lstKey1", "700");
+        Object value= listOperations.range("lstKey1", 0, -1);
+        System.out.println(value);
+        //从list集合取数据
+        Object v1=listOperations.leftPop("lstKey1");//lpop
+        System.out.println("left.pop.0="+v1);
+        value= listOperations.range("lstKey1", 0, -1);
+        System.out.println(value);
+    }
+
+    @Test
+    void testSetOper(){
+        SetOperations setOperations=redisTemplate.opsForSet();
+        setOperations.add("setKey1", "A","B","C","C");
+        Object members=setOperations.members("setKey1");
+        System.out.println("setKeys="+members);
+        //........
+    }
+
+    @Test
+    void testFlushdb(){
+        redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                //redisConnection.flushDb();
+                redisConnection.flushAll();
+                return "flush ok";
+            }
+        });
     }
 
     @Test
